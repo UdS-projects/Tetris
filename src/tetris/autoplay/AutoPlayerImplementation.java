5021e5ca0deb3536e46beb5523f5ba81b2ab0801
Fatih Demirkan <s8fademi@stud.uni-saarland.de>
2019-06-29 14:14:31 +0200

    Move.DOWN

---
+++ src/tetris/autoplay/AutoPlayerImplementation.java
@@ -0,0 +1,39 @@
+package tetris.autoplay;
+
+public class AutoPlayerImplementation implements AutoPlayer
+{
+	public AutoPlayerImplementation()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void rowsCompleted()
+	{
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void piecePositionChanged()
+	{
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void pieceLanded()
+	{
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void gameOver()
+	{
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public Move getMove()
+	{
+		return Move.DOWN;
+	}
+}
